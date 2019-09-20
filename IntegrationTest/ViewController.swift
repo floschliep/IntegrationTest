758aa6b06d218078e5577bf4dc428564519b6f00
Florian Schliep <florian.schliep@sumup.com>
2019-09-20 14:14:22 +0200

    Initial Commit

---
+++ IntegrationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntegrationTest
+//
+//  Created by Florian Schliep on 20.09.19.
+//  Copyright © 2019 SumUp Services GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
